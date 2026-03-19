ac273212764869dbdd347733ff7a59ac910880f6
이지영 <jiyoung.lee@nota.ai>
2026-03-19 23:37:00 +0900

    Update version to 0.1.0

--- Casks/quicktranslate.rb
+++ Casks/quicktranslate.rb
@@ -1,5 +1,5 @@
 cask "quicktranslate" do
-  version "1.0.0"
+  version "0.1.0"
   sha256 :no_check
 
   url "https://github.com/flatto-ai/quick_translator/releases/download/v#{version}/QuickTranslate.dmg"
